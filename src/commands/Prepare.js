8d36a94276b7ceeb655b35033f0fedde79f1b297
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-02-24 15:23:17 +0100

    Allow older pip versions so Travis won't install (and won't need sudo)

--- src/commands/Prepare.js
+++ src/commands/Prepare.js
@@ -127,7 +127,7 @@ class Prepare extends Command {
       name: 'pip',
       exe: 'pip',
       version: '7.1.2',
-      range: `>= {{{self.version}}}`,
+      range: `>= 6.0.0`,
       cmdVersion: '{{{self.exe}}} --version',
       versionTransformer (stdout) {
         const version = `${stdout}`.trim().split('\n')[0].split(/\s+/)[1].replace('v', '')
