752bf7b32eead0c1e1e88d541802808b57965a89
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-04-13 12:36:40 +0200

    Fix Remote ssh bug using `SHELLARG_PREPEND_AS_IS`

--- src/Ssh.js
+++ src/Ssh.js
@@ -19,9 +19,9 @@ class Ssh extends App {
     defaults.args['i'] = this.runtime.config.global.ssh.privatekey_file
     defaults.args['l'] = this.runtime.config.global.ssh.user
 
-    defaults.args['-oUserKnownHostsFile=/dev/null'] = constants.SHELLARG_BOOLEAN_FLAG
-    defaults.args['-oCheckHostIP=no'] = constants.SHELLARG_BOOLEAN_FLAG
-    defaults.args['-oStrictHostKeyChecking=no'] = constants.SHELLARG_BOOLEAN_FLAG
+    defaults.args['-oUserKnownHostsFile=/dev/null'] = constants.SHELLARG_PREPEND_AS_IS
+    defaults.args['-oCheckHostIP=no'] = constants.SHELLARG_PREPEND_AS_IS
+    defaults.args['-oStrictHostKeyChecking=no'] = constants.SHELLARG_PREPEND_AS_IS
 
     if (this.runtime.init.cliargs.verbose) {
       defaults.args['vvvv'] = constants.SHELLARG_BOOLEAN_FLAG
